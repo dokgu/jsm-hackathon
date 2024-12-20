e75afca11e76cd8662d2bdeef7d2af047f5de5c8
Patrick Gregorio <patrickvgregorio@gmail.com>
2024-12-20 09:50:04 -0700

    Project setup

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,19 +1,17 @@
-import { SiteFooter } from "@/components/site-footer";
-import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from '@/components/site-footer';
+import { SiteHeader } from '@/components/site-header';
 
 interface MarketingLayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-export default async function MarketingLayout({
-  children,
-}: MarketingLayoutProps) {
-  return (
-    <>
-      {/* <SiteBanner /> */}
-      <SiteHeader />
-      <main className="mx-auto flex-1 overflow-hidden">{children}</main>
-      <SiteFooter />
-    </>
-  );
+export default async function MarketingLayout({ children }: MarketingLayoutProps) {
+	return (
+		<>
+			{/* <SiteBanner /> */}
+			<SiteHeader />
+			<main className='mx-auto flex-1 overflow-hidden'>{children}</main>
+			<SiteFooter />
+		</>
+	);
 }
