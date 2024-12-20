54799f07c3e0593785ec34e435943a77f7dd623e
Patrick Gregorio <patrickvgregorio@gmail.com>
2024-12-20 15:09:57 -0700

    Set up Visitor Tracking on the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import Head from 'next/head';
+import Script from 'next/script';
 import { Inter as FontSans } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
@@ -18,12 +18,26 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang='en' suppressHydrationWarning>
-			<Head>
-				<title>JSM Hackathon</title>
+			<head>
 				<meta name='description' content='All-in-one platform for startups.' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/icon.png' />
-			</Head>
+				<Script async defer src='https://app.visitortracking.com/assets/js/tracer.js' />
+				<Script
+					id='visitor-tracking'
+					dangerouslySetInnerHTML={{
+						__html: `
+							function init_tracer() {
+								var tracer = new Tracer({
+									websiteId : 'e6e1be1d-1350-49ef-81d2-56f77ebe6d91',
+									async : true,
+									debug : false
+								});
+							}
+						`
+					}}
+				/>
+			</head>
 			<body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
 				<ThemeProvider attribute='class' defaultTheme='dark' disableTransitionOnChange>
 					{children}
