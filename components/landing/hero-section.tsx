e6a5830bcb8e85036811f79166e603a49a732460
Patrick Gregorio <patrickvgregorio@gmail.com>
2024-12-20 13:52:13 -0700

    Fixed hero section copy to be more in line with the service being provided

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -21,10 +21,10 @@ export default function HeroSection() {
 			</div>
 			<h1 className='bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]'>
 				JSM Hackathon is the new way
-				<br className='hidden md:block' /> to quickstart your ideas.
+				<br className='hidden md:block' /> to manage your business.
 			</h1>
 			<p className='mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]'>
-				Beautifully designed, animated components and templates built with
+				Beautifully designed, crafted, implemented, and built with the best tech stack
 				<br className='hidden md:block' /> Tailwind CSS, React, and Framer Motion.
 			</p>
 			<Button className='translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]'>
